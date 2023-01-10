6d5a581361322b0271b366699b75d603d04a07d5
Lukas Dobbelsteen <l.dobbelsteen@gmail.com>
2023-01-10 23:44:36 +0100

    Deeper implementation of triangulation

--- algorithms/triangulate.py
+++ algorithms/triangulate.py
@@ -1,23 +1,21 @@
-from datastructures.DCEL import DCEL, Vertex
+from datastructures.DCEL import DCEL, Vertex, Face, HalfEdge
 
-# Triangulate a y-monotone partitioned polygon. The polygon
-# should be represented by a double-connected egde list.
+
 def triangulate_monotone_polygon(dcel: DCEL):
+    """
+    Triangulate a y-monotone partitioned polygon.
+    """
 
     # Triangulate each y-monotone partition
     for face in dcel.interior_faces():
-        
-        # Get face's vertices and represent them by a tuple. The first value
-        # is the vertex and the second value indicates whether they are on the
-        # left boundary (false) or the right boundary (true). TODO: implement
-        # NOTE: think about how we can efficiently extract vertices from a face?
-        # NOTE: think about how we can efficiently determine which side it is on?
-        vertices: list[tuple[Vertex, bool]]  = []
-        n_vertices = len(vertices)
 
-        # Sort the vertices by their y-coordinates.
-        # NOTE: could this step somehow be avoided?
-        vertices.sort(key=lambda x: x.y)
+        # Extract the left and right boundaries
+        start = highest_leftmost(face.outer_component)
+        left_boundary, right_boundary = extract_boundaries(start)
+
+        # Merge the boundaries
+        vertices = merge_boundaries(left_boundary, right_boundary)
+        n_vertices = len(vertices)
 
         # Initialize stack with first two points
         stack = []
@@ -36,7 +34,7 @@ def triangulate_monotone_polygon(dcel: DCEL):
                 stack.append(vertices[i])
             else:
                 vertex = stack.pop()
-                while len(stack) > 0 and True: # TODO: vertex sees top of stack
+                while len(stack) > 0 and True:  # TODO: vertex sees top of stack condition
                     vertex = stack.pop()
                     dcel.insert_edge(vertices[i], vertex, face)
                 stack.append(vertex)
@@ -48,3 +46,41 @@ def triangulate_monotone_polygon(dcel: DCEL):
         while len(stack) > 1:
             vertex = stack.pop()
             dcel.insert_edge(vertices[-1], vertex, face)
+
+
+def highest_leftmost(edge: HalfEdge) -> HalfEdge:
+    """
+    Locate the half edge with the highest and leftmost origin in a half cycle.
+    """
+
+    best = edge
+    current = edge.next
+    while current != edge:
+        if current.origin.y >= best.origin.y:
+            if current.origin.y > best.origin.y:
+                best = current
+            elif current.origin.x < best.origin.x:
+                best = current
+        current = current.next
+    return best
+
+
+def extract_boundaries(highest_leftmost: HalfEdge) -> tuple[list[Vertex], list[Vertex]]:
+    """
+    Extract the left and right boundaries of a y-monotone polygon using its
+    highest and leftmost vertex.
+    """
+
+    # TODO: implement
+    return ([], [])
+
+
+def merge_boundaries(left_boundary: list[Vertex], right_boundary: list[Vertex]) -> list[tuple[Vertex, bool]]:
+    """
+    Merge boundaries into a list of vertices sorted decreasingly by their
+    y-coordinates. Vertices are accompanied by a boolean indicating whether
+    they are on the left or right boundary (true = left, false = right).
+    """
+
+    # TODO: implement
+    return []
