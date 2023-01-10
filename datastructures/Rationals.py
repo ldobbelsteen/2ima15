03242a12e7d48e2e1ee82599c9b2b52ce90dd3fa
JeroenLamme <j.s.k.lamme@student.tue.nl>
2023-01-10 10:40:04 +0100

    updated rationals

--- datastructures/Rationals.py
+++ datastructures/Rationals.py
@@ -1,7 +1,10 @@
+import math
+
 class Rationals:
     def __init__(self,numerator,denominator=1):
-        self.num = numerator
-        self.den = denominator
+        gcd = math.gcd(self.num ,self.den)
+        self.num = numerator // gcd
+        self.den = denominator // gcd
     
     def __eq__(self, other):
         return self.num * other.den == other.num * self.den 
@@ -17,3 +20,11 @@ class Rationals:
 
     def __lt__(self, other):
         return self.num * other.den < other.num * self.den 
+
+    def __mul__(self, other):
+        return Rationals(self.num * other.num, self.den * other.den)
+
+    __rmul__ = __mul__
+
+    def __str__(self):
+        return '{}/{}'.format(self.num,self.den)
