f68322a24cab9af0327d24cb7b1a622da345776a
DAAN BOELHOUWERS <daanboelhouwers@gmail.com>
2023-01-15 23:06:06 +0100

    Modify tree such that it supports queries for edge left of point

--- datastructures/Edgebst.py
+++ datastructures/Edgebst.py
@@ -9,8 +9,8 @@ def xOfEdge(edge, y):
         return rat(edge.origin.x)
     # Edge is horizontal:
     elif edge.origin.y == edge.twin.origin.y:
+        return rat(max(edge.origin.x, edge.twin.origin.x))
     # Edge is not horizontal nor vertical:
-        return rat(max(edge.origin.x, edge.twin.origin.x)) # TODO: I don't know if this is correct
     else:
         b = rat(edge.origin.y) - slope(edge) * rat(edge.origin.x)
         xEdge = (rat(y) - b) / slope(edge)
@@ -26,14 +26,16 @@ def leftOfVertex(edge,vertex):
 
 
 class EdgebstNode:
-    def __init__(self, val=None):
+    def __init__(self, val=None, right_end=None):
         self.left = None
         self.right = None
         self.val = val
+        self.right_end = right_end
 
-    def insert(self, val, y):
+    def insert_interval(self, val, right_end, y):
         if not self.val:
             self.val = val
+            self.right_end = right_end
             return
 
         if self.val == val or self.val == self.val.twin:
@@ -41,27 +43,30 @@ class EdgebstNode:
 
         if edgeSmaller(val, self.val, y):
             if self.left:
-                self.left.insert(val, y)
+                self.left.insert_interval(val, right_end, y)
                 return
-            self.left = EdgebstNode(val)
+            self.left = EdgebstNode(val, right_end)
             return
 
         if self.right:
-            self.right.insert(val, y)
+            self.right.insert_interval(val, right_end, y)
             return
-        self.right = EdgebstNode(val)
+        self.right = EdgebstNode(val, right_end)
 
-    def delete(self, val, y):
+    def delete_subfunction(self, val, y):
         if self == None:
             return self
-        if edgeSmaller(val,self.val, y):
+        # Element is located in left subtree
+        if edgeSmaller(val, self.val, y):
             if self.left:
-                self.left = self.left.delete(val, y)
+                self.left = self.left.delete_subfunction(val, y)
             return self
-        if not edgeSmaller(val,self.val, y) and val != self.val and val != self.val.twin:
+        # Element is locate in right subtree
+        if not edgeSmaller(val, self.val, y) and val != self.val and val != self.val.twin:
             if self.right:
-                self.right = self.right.delete(val, y)
+                self.right = self.right.delete_subfunction(val, y)
             return self
+        # Current element should be deleted
         if self.right == None:
             return self.left
         if self.left == None:
@@ -70,17 +75,130 @@ class EdgebstNode:
         while min_larger_node.left:
             min_larger_node = min_larger_node.left
         self.val = min_larger_node.val
-        self.right = self.right.delete(min_larger_node.val, y)
+        self.right_end = min_larger_node.right_end
+        self.right = self.right.delete_subfunction(min_larger_node.val, y)
         return self
+
+    def range_query(self, x, y):
+        """
+        returns a tuple consisting of the endpoints of the range in which the querypoint is contained
+        """
+        if not self.val:
+            return None
+        if xOfEdge(self.val, y) < x and (not self.right_end or x < xOfEdge(self.right_end, y)):
+            return (self.val, self.right_end)
+        if xOfEdge(self.val, y) > x:
+            if self.left:
+                return self.left.range_query(x, y)
+            else:
+                # The query point lies left of the leftmost interval
+                return None
+        if xOfEdge(self.right_end, y) < x:
+            return self.right.range_query(x, y)
+        
+    def right_end_query(self, val, y):
+        """
+        given the left end of an interval, returns the right end of that interval
+        """
+        if self.val == val or self.val == val.twin:
+            return self.right_end
+        if xOfEdge(self.val, y) > xOfEdge(val, y):
+            return self.left.right_end_query(val, y)
+        if xOfEdge(self.val, y) < xOfEdge(val, y):
+            return self.right.right_end_query(val, y)
     
+    def left_end_query(self, right_end, y):
+        """
+        given the right end of an interval, returns the left end of that interval
+        """
+        if self.right_end == right_end or self.right_end == right_end.twin:
+            return self.val
+        if not self.right_end or xOfEdge(self.right_end, y) > xOfEdge(right_end, y):
+            if not self.left:
+                return None
+            return self.left.left_end_query(right_end, y)
+        if xOfEdge(self.right_end, y) < xOfEdge(right_end, y):
+            return self.right.left_end_query(right_end, y)
+
+    def get_left_most_edge(self):
+        if not self.left:
+            return self.val
+        else:
+            return self.left.get_left_most_edge()
+
+    def insert(self, val, y):
+        if self.val == None:
+            self.insert_interval(val, None, y)
+            return self
+        else:
+            # First query the interval in which the new edge is contained
+            interval = self.range_query(xOfEdge(val, y), y)
+            if not interval:
+                # The edge we are inserting is the leftmost edge, i.e. it is not contained in any existing intervals
+                right_end = self.get_left_most_edge()
+                self.insert_interval(val, right_end, y)
+                return self
+            else:
+                old_val, old_right_end = interval
+                # Delete the interval from the tree and replace it with two new ones
+                t = self.delete_subfunction(old_val, y)
+                if not t:
+                    t = EdgebstNode()
+                t.insert_interval(old_val, val, y)
+                t.insert_interval(val, old_right_end, y)
+                return t
+
+    def delete(self, val, y):
+        # Find right end of interval being deleted
+        right_end = self.right_end_query(val, y)
+        # Find left edge of interval whose left end now changed and replace it
+        left_end = self.left_end_query(val, y)
+        if not left_end:
+            # Interval deleted was leftmost interval, no interval to replace
+            t = self.delete_subfunction(val, y)
+            if not t:
+                t = EdgebstNode()
+            return t
+        else:
+            # Delete the edge that we want to delete
+            t = self.delete_subfunction(val, y)
+            # Replace the edge that changed because of the deletion
+            t = t.delete_subfunction(left_end, y)
+            if not t:
+                t = EdgebstNode()
+            t.insert_interval(left_end, right_end, y)
+            return t
+
+
+    
+    # ===== DEBUGGING FUNCTIONS =============================================
+
     def print_nodes(self):
         if not self.val:
             return
-        print(f"(({self.val.origin.x}, {self.val.origin.y}), ({self.val.twin.origin.x}, {self.val.twin.origin.y}))")
+        print(f"left: (({self.val.origin.x}, {self.val.origin.y}), ({self.val.twin.origin.x}, {self.val.twin.origin.y}))")
+        if self.right_end:
+            print(f"right: (({self.right_end.origin.x}, {self.right_end.origin.y}), ({self.right_end.twin.origin.x}, {self.right_end.twin.origin.y}))")
+        else:
+            print("right: None")
         if self.left:
             self.left.print_nodes()
         if self.right:
             self.right.print_nodes()
+
+    def nodes_to_string(self):
+        if not self.val:
+            return
+        str_rep = f"(({self.val.origin.x}, {self.val.origin.y}), ({self.val.twin.origin.x}, {self.val.twin.origin.y}))"
+        if self.left:
+            str_rep = str_rep + " : (left: " + self.left.nodes_to_string()
+        else:
+            str_rep = str_rep + "(left: ()"
+        if self.right:
+            str_rep = str_rep + "right: " + self.right.nodes_to_string() +")"
+        else:
+            str_rep = str_rep + "right ())"
+        return str_rep
     
     def node_count(self):
         count = 0
@@ -89,13 +207,29 @@ class EdgebstNode:
         if self.right:
             count += self.right.node_count()
         return count
+
+    def is_valid_bst_val(self, y):
+        valid = True
+        if self.left:
+            valid = valid and self.left.is_valid_bst_val(y) and xOfEdge(self.left.val, y) < xOfEdge(self.val, y)
+        if self.right:
+            valid = valid and self.right.is_valid_bst_val(y) and xOfEdge(self.right.val, y) > xOfEdge(self.val, y)
+        return valid
     
-    def leftEdgeFinder(self, vertex):
-        if leftOfVertex(self.val, vertex):
-            if self.right and leftOfVertex(self.right.val, vertex):
-                return self.right.leftEdgeFinder(vertex)
-            else:
-                return self.val
-        else:
-            return self.left.leftEdgeFinder(vertex)
-    
+    def is_valid_bst_right_end(self, y):
+        valid = True
+        if self.left:
+            valid = valid and self.left.is_valid_bst_right_end(y) and (not self.right_end or xOfEdge (self.left.right_end, y) < xOfEdge(self.right_end, y))
+        if self.right:
+            valid = valid and self.right.is_valid_bst_right_end(y) and (not self.right.right_end or xOfEdge(self.right.right_end, y) > xOfEdge(self.right_end, y))
+        return valid
+
+    def exists_right_none(self):
+        exists = True
+        if self.right_end:
+            exists = False
+        if self.right:
+            exists = exists or self.right.exists_right_none()
+        if self.left:
+            exists = exists or self.left.exists_right_none()
+        return exists
